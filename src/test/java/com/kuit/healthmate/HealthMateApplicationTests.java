ae91dd5abdab6ac94a497ee407ee61bdc0efb0b6
kamothi <rhkddlf7911@naver.com>
2024-07-12 20:24:17 +0900

    init

---
+++ src/test/java/com/kuit/healthmate/HealthMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuit.healthmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
