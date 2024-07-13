7192ee7dad595bbae8cadbc1408c103e4d7ad47b
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-07-13 21:43:13 +0900

    feat: SupplementTime 구현

--- src/main/java/com/kuit/healthmate/domain/SupplementTime.java
+++ src/main/java/com/kuit/healthmate/domain/SupplementTime.java
@@ -1,4 +1,28 @@
 package com.kuit.healthmate.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalTime;
+
+@Entity
+@Table
 public class SupplementTime {
+    @Id
+    @GeneratedValue
+    @Column(name = "supplement_time_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="supplement_id")
+    private Supplement supplement;
+
+    private TimeSlot timeSlot;
+
+    private LocalTime time;
 }
