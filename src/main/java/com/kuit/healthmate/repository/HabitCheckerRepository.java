f69c9fdc4fb3b4bcd9fe7a8602170f7f08936797
park99999 <sungung1030@gmail.com>
2024-07-13 20:47:27 +0900

    fix : habitRepository 수정

--- src/main/java/com/kuit/healthmate/repository/HabitCheckerRepository.java
+++ src/main/java/com/kuit/healthmate/repository/HabitCheckerRepository.java
@@ -2,6 +2,8 @@ package com.kuit.healthmate.repository;
 
 import com.kuit.healthmate.domain.HabitChecker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HabitCheckerRepository extends JpaRepository<HabitChecker,Long> {
 }
--- src/main/java/com/kuit/healthmate/repository/HabitRepository.java
+++ src/main/java/com/kuit/healthmate/repository/HabitRepository.java
@@ -2,6 +2,8 @@ package com.kuit.healthmate.repository;
 
 import com.kuit.healthmate.domain.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HabitRepository  extends JpaRepository<Habit, Long> {
 }
