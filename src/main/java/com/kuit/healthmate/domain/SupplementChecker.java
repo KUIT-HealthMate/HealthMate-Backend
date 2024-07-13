8dc0f47a40122e76328fb87a9d0dc51052765467
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-07-13 21:54:45 +0900

    feat: SupplementChecker 구현

--- src/main/java/com/kuit/healthmate/domain/SupplementChecker.java
+++ src/main/java/com/kuit/healthmate/domain/SupplementChecker.java
@@ -1,4 +1,29 @@
 package com.kuit.healthmate.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table
 public class SupplementChecker {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "supplement_checker_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="supplement_id")
+    private Supplement supplement;
+
+    private LocalDate checkDate;
+    private TimeSlot timeSlot;
+    private Boolean status;
 }
