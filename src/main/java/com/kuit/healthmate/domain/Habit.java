b98f01e4e190de809118b0af1ba1ad8b16fce78c
park99999 <sungung1030@gmail.com>
2024-07-13 15:37:16 +0900

    fix : habit entity 수정

--- src/main/java/com/kuit/healthmate/domain/Habit.java
+++ src/main/java/com/kuit/healthmate/domain/Habit.java
@@ -1,8 +1,13 @@
 package com.kuit.healthmate.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,6 +18,22 @@ public class Habit {
     @Column(name = "habit_id")
     private Long id;
 
+    @Column(length = 25,nullable = false)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(length = 25,nullable = false)
+    @ColumnDefault()
+    private String status;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @ColumnDefault("-1")
+    private Long selected_day; //0000000. 0000001, 1010111
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id")
