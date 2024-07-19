abc77cbd1bbff96d4199b99492c4f1b377d9bd89
kamothi <rhkddlf7911@naver.com>
2024-07-19 16:10:15 +0900

    feat: 성공 response 생성

---
+++ src/main/java/com/kuit/healthmate/global/response/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.kuit.healthmate.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.kuit.healthmate.global.response.ExceptionResponseStatus.SUCCESS;
+
+
+public class ApiResponse<T> {
+    private final int code;
+    private final int status;
+    private final String message;
+    private final LocalDateTime timestamp;
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    private final T result;
+
+    public ApiResponse(T result) {
+        this.code = SUCCESS.getCode();
+        this.status = SUCCESS.getStatus();
+        this.message = SUCCESS.getMessage();
+        this.timestamp = LocalDateTime.now();
+        this.result = result;
+    }
+}
