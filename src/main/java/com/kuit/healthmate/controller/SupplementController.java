393054abbbfd2dd19f39138b706ffb1796d906c1
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-07-22 13:49:33 +0900

    feat: 영양제 챌린지 등록부분 컨트롤러 구현

--- src/main/java/com/kuit/healthmate/controller/SupplementController.java
+++ src/main/java/com/kuit/healthmate/controller/SupplementController.java
@@ -1,5 +1,6 @@
 package com.kuit.healthmate.controller;
 
+import com.kuit.healthmate.dto.supplement.SupplementRegisterRequest;
 import com.kuit.healthmate.dto.supplement.SupplementResponse;
 import com.kuit.healthmate.dto.supplement.SupplementUpdateRequest;
 import com.kuit.healthmate.global.response.ApiResponse;
@@ -9,10 +10,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @RestController
@@ -26,6 +29,12 @@ public class SupplementController {
     @GetMapping("/{userId}")
     public ApiResponse<List<SupplementResponse>> getSupplementByUserId(@PathVariable Long userId) {
         return new ApiResponse<>(supplementService.getSupplementChallengesByUserId(userId));
+    }
+
+    @PostMapping("/register")
+    public void registerSupplement(@RequestBody SupplementRegisterRequest supplementRegisterRequest) {
+        supplementService.registerSupplement(supplementRegisterRequest);
+        return;
     }
 
     @PutMapping("/{supplementId}")
