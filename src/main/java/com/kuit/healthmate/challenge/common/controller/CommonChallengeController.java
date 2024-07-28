4c42da54ee56d20227db0dc472bb32e4c003b55c
kamothi <rhkddlf7911@naver.com>
2024-07-28 16:27:37 +0900

    feat: common challenge controller 생성

--- src/main/java/com/kuit/healthmate/challenge/common/controller/CommonChallengeController.java
+++ src/main/java/com/kuit/healthmate/challenge/common/controller/CommonChallengeController.java
@@ -1,20 +1,20 @@
 package com.kuit.healthmate.challenge.common.controller;
 
+import com.kuit.healthmate.challenge.common.dto.ChallengeRequest;
 import com.kuit.healthmate.challenge.common.dto.ChallengeResponse;
 import com.kuit.healthmate.challenge.common.service.CommonChallengeService;
 import com.kuit.healthmate.challenge.common.service.CommonChallengeServiceImpl;
 import com.kuit.healthmate.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -28,24 +28,47 @@ public class CommonChallengeController {
     /**
      * 특정 날짜의 챌린지를 가져옵니다.
      *
-     * @param date 조회할 날짜 (예: 2024.02.25)
+     * @param date 조회할 날짜 (예: 2024-02-25)
      * @return 해당 날짜의 챌린지 정보
      */
     @GetMapping("/by-day")
     public ApiResponse<ChallengeResponse> getChallengesByDay(@RequestParam String date) {
+        Long userId = null;
         // userId는?
         log.info(date);
-        return new ApiResponse<>(commonChallengeService.getChallengesForDay(LocalDate.parse(date, FORMATTER)));
+        return new ApiResponse<>(commonChallengeService.getChallengesForDay(userId,LocalDate.parse(date, FORMATTER)));
     }
 
     /**
-     * 금일의 챌린지를 가져옵니다.
+     * 금주의 챌린지를 가져옵니다.
      * @return 금일의 챌린지 정보
      */
-    @GetMapping("/today")
-    public ApiResponse<ChallengeResponse> getTodayChallenges() {
+    @GetMapping("/today/{userId}")
+    public ApiResponse<ChallengeResponse> getChallengesByToday(@PathVariable Long userId) {
         log.info("");
-        return new ApiResponse<>(commonChallengeService.getChallengesForToday());
+        return new ApiResponse<>(commonChallengeService.getChallengesForToday(userId));
+    }
+
+    /**
+     * 금주의 챌린지를 가져옵니다.
+     * @return 금일의 챌린지 정보
+     */
+    @GetMapping("/week")
+    public ApiResponse<List<ChallengeResponse>> getChallengesByWeek(@RequestBody ChallengeRequest challengeRequest) {
+        log.info("");
+        return new ApiResponse<>(commonChallengeService.getChallengesForWeek(challengeRequest.getUserId(),
+                challengeRequest.getStartDate(), challengeRequest.getEndDate()));
+    }
+
+    /**
+     * 이번 달의 챌린지를 가져옵니다.
+     * @return 금일의 챌린지 정보
+     */
+    @GetMapping("/month")
+    public ApiResponse<List<ChallengeResponse>> getChallengesByMonth(@RequestBody ChallengeRequest challengeRequest) {
+        log.info("");
+        return new ApiResponse<>(commonChallengeService.getChallengesForMonth(challengeRequest.getUserId(),
+                challengeRequest.getStartDate(), challengeRequest.getEndDate()));
     }
 
 }
