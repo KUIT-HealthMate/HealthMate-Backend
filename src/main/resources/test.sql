b956ab10579fdfa96ba20ac854daf6767f0e1273
kamothi <rhkddlf7911@naver.com>
2024-07-28 17:35:08 +0900

    test: test를 위한 dml 추가

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -5,8 +5,12 @@ insert into users(user_id, email, nickname, status, created_at)
 values (2, 'test2@gmail.com', 'test2', 'GENERAL', current_timestamp);
 
 insert into supplements (supplement_id, user_id, name, after_meal, selected_day, breakfast, lunch, dinner,
-                          status, created_at, updated_at)
+                         status, created_at, updated_at)
 values (1, 1, 'test1', '30', '0000000', true, true, true, 'ACTIVE', current_timestamp(), current_timestamp());
+
+insert into supplements (supplement_id, user_id, name, after_meal, selected_day, breakfast, lunch, dinner,
+                         status, created_at, updated_at)
+values (3, 1, 'test3', '30', '0000000', true, true, true, 'ACTIVE', current_timestamp(), current_timestamp());
 
 insert into supplements (supplement_id, user_id, name, after_meal, selected_day, breakfast, lunch, dinner,
                          status, created_at, updated_at)
@@ -17,3 +21,6 @@ values (1, 1, 'LUNCH', true, current_date);
 
 insert Into supplement_checker(supplement_checker_id, supplement_id, time_slot, status, check_date)
 values (2, 1, 'LUNCH', true, '2012-12-14');
+
+insert Into supplement_checker(supplement_checker_id, supplement_id, time_slot, status, check_date)
+values (3, 2, 'LUNCH', true, current_date);
