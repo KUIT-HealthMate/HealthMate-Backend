ab4a94c88ca5b85f934a06643f5625a95f7e9f29
kamothi <rhkddlf7911@naver.com>
2024-07-28 16:55:51 +0900

    refact: param -> requestbody로 변경

--- src/main/java/com/kuit/healthmate/challenge/common/controller/CommonChallengeController.java
+++ src/main/java/com/kuit/healthmate/challenge/common/controller/CommonChallengeController.java
@@ -1,21 +1,17 @@
 package com.kuit.healthmate.challenge.common.controller;
 
-import com.kuit.healthmate.challenge.common.dto.ChallengeByPeriodResponse;
-import com.kuit.healthmate.challenge.common.dto.ChallengeRequest;
-import com.kuit.healthmate.challenge.common.dto.ChallengeResponse;
+import com.kuit.healthmate.challenge.common.dto.request.ChallengeByTodayRequest;
+import com.kuit.healthmate.challenge.common.dto.response.ChallengeByPeriodResponse;
+import com.kuit.healthmate.challenge.common.dto.request.ChallengeRequest;
+import com.kuit.healthmate.challenge.common.dto.response.ChallengeResponse;
 import com.kuit.healthmate.challenge.common.service.CommonChallengeService;
-import com.kuit.healthmate.challenge.common.service.CommonChallengeServiceImpl;
 import com.kuit.healthmate.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.query.Param;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,24 +20,20 @@ import java.util.List;
 public class CommonChallengeController {
     private final CommonChallengeService commonChallengeService;
     // 불변 객체 계속 사용할 변수임.
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     /**
      * 특정 날짜의 챌린지를 가져옵니다.
      *
-     * @param date 조회할 날짜 (예: 2024-02-25)
      * @return 해당 날짜의 챌린지 정보
      */
     @GetMapping("/by-day")
-    public ApiResponse<ChallengeResponse> getChallengesByDay(@RequestParam String date) {
-        Long userId = null;
-        // userId는?
-        log.info(date);
-        return new ApiResponse<>(commonChallengeService.getChallengesForDay(userId,LocalDate.parse(date, FORMATTER)));
+    public ApiResponse<ChallengeResponse> getChallengesByDay(@RequestBody ChallengeByTodayRequest challenge) {
+        return new ApiResponse<>(commonChallengeService.getChallengesForDay(challenge.getUserId(), challenge.getDate()));
     }
 
     /**
-     * 금주의 챌린지를 가져옵니다.
+     * 오늘의 챌린지를 가져옵니다.
      * @return 금일의 챌린지 정보
      */
     @GetMapping("/today/{userId}")
