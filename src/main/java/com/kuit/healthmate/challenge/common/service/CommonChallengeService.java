9591f63bf47dee9dc371ff29299ce26e74b9a725
kamothi <rhkddlf7911@naver.com>
2024-07-28 16:56:14 +0900

    feat: common challenge service 로직 생성

--- src/main/java/com/kuit/healthmate/challenge/common/service/CommonChallengeService.java
+++ src/main/java/com/kuit/healthmate/challenge/common/service/CommonChallengeService.java
@@ -1,28 +1,28 @@
 package com.kuit.healthmate.challenge.common.service;
 
-import com.kuit.healthmate.challenge.common.dto.ChallengeResponse;
+import com.kuit.healthmate.challenge.common.dto.response.ChallengeByPeriodResponse;
+import com.kuit.healthmate.challenge.common.dto.response.ChallengeResponse;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public interface CommonChallengeService {
     /*
      * 해당 요일 챌린지 가져오기(챌린지 리스트와 수행여부)
      * */
-    public ChallengeResponse getChallengesForDay(LocalDate localDate);
+    public ChallengeResponse getChallengesForDay(Long userId,LocalDate localDate);
 
     /*
      * 금일 챌린지 가져오기
      * */
-    public ChallengeResponse getChallengesForToday();
+    public ChallengeResponse getChallengesForToday(Long userId);
 
     /*
     *  주간 챌린지 가져오기(챌린지 리스트와 수행 여부, 성취도)
     * */
-    public ChallengeResponse getChallengesForMonth();
+    public ChallengeByPeriodResponse getChallengesForMonth(Long userId, LocalDate startDate, LocalDate endDate);
 
     /*
     * 월간 챌린지 가져오기(챌린지 리스트와 수행 여부, 성취도)
     * */
-    public ChallengeResponse getChallengesForWeek();
+    public ChallengeByPeriodResponse getChallengesForWeek(Long userId, LocalDate startDate, LocalDate endDate);
 }
