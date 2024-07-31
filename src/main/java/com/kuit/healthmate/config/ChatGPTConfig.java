683f5ab6cea16e11992163e9cd6c517df567db75
kamothi <rhkddlf7911@naver.com>
2024-07-31 23:31:58 +0900

    refact: annotaion 변경

--- src/main/java/com/kuit/healthmate/config/ChatGPTConfig.java
+++ src/main/java/com/kuit/healthmate/config/ChatGPTConfig.java
@@ -4,9 +4,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@Component
 public class ChatGPTConfig {
     @Value("${openai.api.key}")
     private String openaiApiKey;
