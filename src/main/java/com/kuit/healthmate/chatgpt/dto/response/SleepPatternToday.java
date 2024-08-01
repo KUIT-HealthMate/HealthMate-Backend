0b908d55767dbd1b70b7706aaeabdc8f1e85cf56
kamothi <rhkddlf7911@naver.com>
2024-08-01 15:51:17 +0900

    feat: meal, sleep response dto 생성

---
+++ src/main/java/com/kuit/healthmate/chatgpt/dto/response/SleepPatternToday.java
@@ -0,0 +1,12 @@
+package com.kuit.healthmate.chatgpt.dto.response;
+
+public class SleepPatternToday {
+    private String description;
+    private String regularity; // 수면 패턴의 규칙성
+    private String sleepQuality; // 수면의 질
+    private String sleepFocus; // 수면의 집중도
+    private int dailySleepPatternScore; // 일간 수면 패턴 점수
+    private int riskScore; // 위험 점수
+    private String riskSymptoms; // 위험 질환
+    private String challenges; // 챌린지 추천
+}
