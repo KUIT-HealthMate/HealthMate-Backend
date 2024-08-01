c9330ea37231774dddd404588d838055b2e9b74c
kamothi <rhkddlf7911@naver.com>
2024-08-01 16:48:34 +0900

    refact: 필드 타입 변경

--- src/main/java/com/kuit/healthmate/chatgpt/dto/response/MealPatternToday.java
+++ src/main/java/com/kuit/healthmate/chatgpt/dto/response/MealPatternToday.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 @Getter
 public class MealPatternToday {
     private String description;
-    private String regularity; // 식사 패턴의 규칙성
-    private String alcoholFrequency; // 음주 빈도
-    private String nutritionIntake; // 영양분 섭취 정도
+    private int regularity; // 식사 패턴의 규칙성
+    private int alcoholFrequency; // 음주 빈도
+    private int nutritionIntake; // 영양분 섭취 정도
     private int dailyMealPatternScore; // 일간 식사 패턴 점수
     private int riskScore; // 위험 점수
     private String riskSymptoms; // 위험 질환
