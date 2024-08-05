a7b3461f8ea573105fbc2fdf7dcc40eb21646428
kamothi <rhkddlf7911@naver.com>
2024-08-05 17:57:34 +0900

    feat: 직렬화 문제 해결 및 쿼리 추가

--- src/main/java/com/kuit/healthmate/diagnosis/life/repository/LifeStyleQuestionnaireRepository.java
+++ src/main/java/com/kuit/healthmate/diagnosis/life/repository/LifeStyleQuestionnaireRepository.java
@@ -2,8 +2,16 @@ package com.kuit.healthmate.diagnosis.life.repository;
 
 import com.kuit.healthmate.diagnosis.life.domain.LifeStyleQuestionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface LifeStyleQuestionnaireRepository extends JpaRepository<LifeStyleQuestionnaire, Long> {
+    // 특정 주에 해당하는 데이터를 조회하기 위한 메서드
+    @Query("SELECT l FROM LifeStyleQuestionnaire l WHERE l.timestamp >= :weekStartDate AND l.timestamp <= :weekEndDate")
+    List<LifeStyleQuestionnaire> findByWeek(LocalDateTime weekStartDate, LocalDateTime weekEndDate);
 }
