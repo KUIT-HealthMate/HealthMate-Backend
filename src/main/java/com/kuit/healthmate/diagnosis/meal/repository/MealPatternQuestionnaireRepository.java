93b06df33eb64e5f1ffa0ad428195e7f05a6dfea
kamothi <rhkddlf7911@naver.com>
2024-08-06 13:02:20 +0900

    feat: 식사, 수면 주간 정보 find query 추가 및 순환참조 문제 해결

--- src/main/java/com/kuit/healthmate/diagnosis/meal/repository/MealPatternQuestionnaireRepository.java
+++ src/main/java/com/kuit/healthmate/diagnosis/meal/repository/MealPatternQuestionnaireRepository.java
@@ -2,8 +2,15 @@ package com.kuit.healthmate.diagnosis.meal.repository;
 
 import com.kuit.healthmate.diagnosis.meal.domain.MealPatternQuestionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface MealPatternQuestionnaireRepository extends JpaRepository<MealPatternQuestionnaire,Long> {
+
+    @Query("SELECT m FROM MealPatternQuestionnaire m WHERE m.timestamp >= :weekStartDate AND m.timestamp <= :weekEndDate")
+    List<MealPatternQuestionnaire> findByWeek(LocalDateTime weekStartDate, LocalDateTime weekEndDate);
 }
