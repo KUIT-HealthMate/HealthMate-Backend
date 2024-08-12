7f3d00087cf7b5e2fd6bdffb7c76e3a3d58e9435
park99999 <sungung1030@gmail.com>
2024-08-12 22:28:11 +0900

    feat : 온보딩 사용자 정보 저장

--- src/main/java/com/kuit/healthmate/user/domain/onboarding/service/OnboardingService.java
+++ src/main/java/com/kuit/healthmate/user/domain/onboarding/service/OnboardingService.java
@@ -3,6 +3,7 @@ package com.kuit.healthmate.user.domain.onboarding.service;
 import com.kuit.healthmate.challenge.supplement.repository.UserRepository;
 import com.kuit.healthmate.global.exception.UserException;
 import com.kuit.healthmate.global.response.ExceptionResponseStatus;
+import com.kuit.healthmate.user.Gender;
 import com.kuit.healthmate.user.domain.User;
 import com.kuit.healthmate.user.domain.onboarding.domain.OnboardingInfo;
 import com.kuit.healthmate.user.domain.onboarding.repository.OnboardingInfoRepository;
@@ -22,6 +23,12 @@ public class OnboardingService {
     public OnboardingInfo saveOnboardingInfo(Long userId, int gender, int ageGroup, List<String> symptoms, int purpose) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(ExceptionResponseStatus.INVALID_USER_ID));
+        switch (gender) {
+            case 1:
+                user.setAdditionalInfo(ageGroup,Gender.FEMALE);
+            case 2:
+                user.setAdditionalInfo(ageGroup,Gender.MALE);
+        }
 
         OnboardingInfo onboardingInfo = OnboardingInfo.builder()
                 .user(user)
