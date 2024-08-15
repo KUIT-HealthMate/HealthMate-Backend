928bacb3fc8dbca1d6b4783555f187f1b461cb54
kamothi <rhkddlf7911@naver.com>
2024-08-15 23:19:00 +0900

    feat: 평균 계산 job 등록

--- src/main/java/com/kuit/healthmate/batch/BatchConfig.java
+++ src/main/java/com/kuit/healthmate/batch/BatchConfig.java
@@ -1,12 +1,15 @@
 package com.kuit.healthmate.batch;
 
 
+import com.kuit.healthmate.batch.analytics.DataAnalysisAndSaveTasklet;
+import com.kuit.healthmate.batch.analytics.FetchScoreTasklet;
 import com.kuit.healthmate.batch.month.FetchMonthDataTasklet;
 import com.kuit.healthmate.batch.month.SendToMonthDataGptAndSaveTasklet;
 import com.kuit.healthmate.batch.month.TransformMonthDataPromptTasklet;
 import com.kuit.healthmate.batch.week.FetchHealthDataTasklet;
 import com.kuit.healthmate.batch.week.SendToGptAndSaveTasklet;
 import com.kuit.healthmate.batch.week.TransformPromptTasklet;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -33,6 +36,10 @@ public class BatchConfig {
     private final FetchMonthDataTasklet fetchMonthDataTasklet;
     private final TransformMonthDataPromptTasklet transformMonthDataPromptTasklet;
     private final SendToMonthDataGptAndSaveTasklet sendToMonthDataGptAndSaveTasklet;
+
+    // 평균
+    private final FetchScoreTasklet fetchScoreTasklet;
+    private final DataAnalysisAndSaveTasklet dataAnalysisAndSaveTasklet;
 
 
     @Bean
@@ -92,6 +99,29 @@ public class BatchConfig {
     public Step sendToMonthDataGptStep() {
         return new StepBuilder("sendToMonthDataGptStep", jobRepository)
                 .tasklet(sendToMonthDataGptAndSaveTasklet, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Job jobAverage() {
+        return new JobBuilder("jobAverage", jobRepository)
+                .start(fetchScoreStep())
+                .next(dataAnalysisAndSaveStep())
+                .next(sendToMonthDataGptStep())
+                .build();
+    }
+
+    @Bean
+    public Step fetchScoreStep() {
+        return new StepBuilder("fetchScoreStep", jobRepository)
+                .tasklet(fetchScoreTasklet, transactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step dataAnalysisAndSaveStep() {
+        return new StepBuilder("dataAnalysisAndSaveStep", jobRepository)
+                .tasklet(dataAnalysisAndSaveTasklet, transactionManager)
                 .build();
     }
 }
