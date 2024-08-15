6d28e23fd495a8175cad106835a99a0120b00332
kamothi <rhkddlf7911@naver.com>
2024-08-15 23:03:53 +0900

    feat: 데이터 처리 task 생성

--- src/main/java/com/kuit/healthmate/batch/analytics/DataAnalysisAndSaveTasklet.java
+++ src/main/java/com/kuit/healthmate/batch/analytics/DataAnalysisAndSaveTasklet.java
@@ -2,13 +2,10 @@ package com.kuit.healthmate.batch.analytics;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kuit.healthmate.chatgpt.util.formatter.week.LifeStyleWeekFomatter;
-import com.kuit.healthmate.chatgpt.util.formatter.week.MealPatternWeekFormatter;
 import com.kuit.healthmate.diagnosis.gpt.domain.GptMonthResult;
 import com.kuit.healthmate.diagnosis.gpt.domain.GptResult;
 import com.kuit.healthmate.diagnosis.gpt.domain.GptWeekResult;
-import com.kuit.healthmate.diagnosis.life.domain.LifeStyleQuestionnaire;
-import com.kuit.healthmate.diagnosis.meal.domain.MealPatternQuestionnaire;
+import com.kuit.healthmate.diagnosis.healthscore.service.UserHealthAverageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepContribution;
@@ -18,17 +15,29 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataAnalysisAndSaveTasklet implements Tasklet {
+    private final UserHealthAverageService userHealthAverageService;
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         log.info("Analysis data...");
-        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();// 초기화
+        int dailyLifeStyleCount = 0;
+        int dailyMealCount = 0;
+        int dailySleepCount = 0;
+        int weekLifeStyleCount = 0;
+        int weekMealCount = 0;
+        int weekSleepCount = 0;
+        int monthLifeStyleCount = 0;
+        int monthMealCount = 0;
+        int monthSleepCount = 0;
+
+        int cnt = 0, cnt2 = 0, cnt3 = 0;
+
 
         String gptTodayJson = (String) chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("today");
         String gptWeekJson = (String) chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("week");
@@ -36,16 +45,42 @@ public class DataAnalysisAndSaveTasklet implements Tasklet {
 
         if(gptTodayJson != null){
             List<GptResult> gptResults = objectMapper.readValue(gptTodayJson,new TypeReference<>() {});
+            for (GptResult gptResult : gptResults) {
+                dailyLifeStyleCount += gptResult.getLifeStyleToday().getLifeStyleScore();
+                dailyMealCount += gptResult.getMealPatternToday().getDailyMealPatternScore();
+                dailySleepCount += gptResult.getSleepPatternToday().getDailySleepPatternScore();
+                cnt++;
+            }
         }
+
         if (gptWeekJson != null) {
             List<GptWeekResult> gptWeekResults = objectMapper.readValue(gptWeekJson,new TypeReference<>() {});
+
+            for (GptWeekResult gptWeekResult : gptWeekResults) {
+                weekLifeStyleCount += gptWeekResult.getLifeStyleToday().getLifeStyleScore();
+                weekMealCount += gptWeekResult.getMealPatternToday().getDailyMealPatternScore();
+                weekSleepCount += gptWeekResult.getSleepPatternToday().getDailySleepPatternScore();
+                cnt2++;
+            }
 
         }
 
         if (gptMonthJson != null) {
             List<GptMonthResult> gptMonthResults = objectMapper.readValue(gptMonthJson,new TypeReference<>() {});
+            for (GptMonthResult gptMonthResult : gptMonthResults) {
+                monthLifeStyleCount += gptMonthResult.getLifeStyleToday().getLifeStyleScore();
+                monthMealCount += gptMonthResult.getMealPatternToday().getDailyMealPatternScore();
+                monthSleepCount += gptMonthResult.getSleepPatternToday().getDailySleepPatternScore();
+                cnt3++;
+            }
 
         }
+
+        userHealthAverageService.updateAverage(dailyLifeStyleCount/cnt,dailyMealCount/cnt,dailySleepCount/cnt,
+                weekLifeStyleCount/cnt2,weekMealCount/cnt2,weekSleepCount/cnt2,
+                monthLifeStyleCount/cnt3,monthMealCount/cnt3,monthSleepCount/cnt3);
+
+
 
         return RepeatStatus.FINISHED;
     }
