c2b8425738994ba8e70209551ea7e78fcb686212
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-08-16 23:17:24 +0900

    refactor: userController 마이페이지 매퍼 메서드 구현

--- src/main/java/com/kuit/healthmate/user/dto/MyPageResponse.java
+++ src/main/java/com/kuit/healthmate/user/dto/MyPageResponse.java
@@ -1,5 +1,7 @@
 package com.kuit.healthmate.user.dto;
 
+import com.kuit.healthmate.user.domain.User;
+import com.kuit.healthmate.user.domain.onboarding.domain.OnboardingInfo;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +14,11 @@ public class MyPageResponse {
     private String profileImage;
     private Integer coin;
     private List<String> health;
+
+    public MyPageResponse(User user, OnboardingInfo onboardingInfo) {
+        this.nickname = user.getNickname();
+        this.profileImage = user.getProfile();
+        this.coin = Math.toIntExact(user.getBalance());
+        this.health = onboardingInfo.getSymptoms();
+    }
 }
