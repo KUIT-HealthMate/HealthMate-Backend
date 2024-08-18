48c9225e9176fdf5da33f63c793387dc19014a47
park99999 <sungung1030@gmail.com>
2024-08-19 00:42:19 +0900

    refact : week 조회

--- src/main/java/com/kuit/healthmate/diagnosis/common/service/DiagnosisService.java
+++ src/main/java/com/kuit/healthmate/diagnosis/common/service/DiagnosisService.java
@@ -48,6 +48,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.WeekFields;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -197,7 +198,7 @@ public class DiagnosisService {
         LocalDate previousWeekMonday = formatDate.minusWeeks(1).with(DayOfWeek.MONDAY);
         LocalDate previousWeekSunday = formatDate.minusWeeks(1).with(DayOfWeek.SUNDAY);
 
-        GptWeekResult gptWeekResult = gptWeekResultRepository.findDiagnosisResultByUserIdAndDate(userId, (long)formatDate.get(WeekFields.ISO.weekOfYear()),(long)formatDate.getYear())
+        GptWeekResult gptWeekResult = gptWeekResultRepository.findDiagnosisResultByUserIdAndDate(userId, (long)previousWeekMonday.get(WeekFields.ISO.weekOfYear()),(long)previousWeekMonday.getYear())
                 .orElseThrow(() -> new DiagnosisException(ExceptionResponseStatus.INVALID_DIAGNOSIS_VALUE, "진단 결과가 존재하지 않습니다"));
         List<UserHealthAverage> userHealthAverages = userHealthAverageService.getAverageByDate(previousWeekMonday,previousWeekSunday);
 
@@ -212,15 +213,16 @@ public class DiagnosisService {
         }
 
         //내 점수 가져오기
-        List<Integer> lifeScores = new ArrayList<>();
-        List<Integer> mealScores = new ArrayList<>();
-        List<Integer> sleepScores = new ArrayList<>();
+        List<Integer> lifeScores = new ArrayList<>(Collections.nCopies(7, 0));
+        List<Integer> mealScores = new ArrayList<>(Collections.nCopies(7, 0));
+        List<Integer> sleepScores = new ArrayList<>(Collections.nCopies(7, 0));
         List<GptResult> gptResults = gptResultRepository.findDiagnosisResultByUserIdAndBetweenDate(userId,previousWeekMonday,previousWeekSunday);
-        for (GptResult item :gptResults){
-            lifeScores.add(item.getLifeStyleToday().getLifeStyleScore());
-            mealScores.add(item.getMealPatternToday().getDailyMealPatternScore());
-            sleepScores.add(item.getSleepPatternToday().getDailySleepPatternScore());
+        for (GptResult gptResult : gptResults) {
+            lifeScores.set(gptResult.getDate().getDayOfWeek().getValue()-1, gptResult.getLifeStyleToday().getLifeStyleScore());
+            mealScores.set(gptResult.getDate().getDayOfWeek().getValue()-1, gptResult.getMealPatternToday().getDailyMealPatternScore());
+            sleepScores.set(gptResult.getDate().getDayOfWeek().getValue()-1, gptResult.getSleepPatternToday().getDailySleepPatternScore());
         }
+
         LifeStyleWeekResponse lifeStyleWeekResponse = new LifeStyleWeekResponse(
                 lifeAverages,lifeScores,gptWeekResult.getLifeStyleToday().getDescription(), gptWeekResult.getLifeStyleToday().getRiskScore(),gptWeekResult.getLifeStyleToday().getRiskSymptoms(),gptWeekResult.getLifeStyleToday().getChallenges()
         );
