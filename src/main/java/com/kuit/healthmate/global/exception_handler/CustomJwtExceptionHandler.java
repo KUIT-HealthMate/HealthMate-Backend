b2121ea4dfc78e598b95e9cad975a0ab9ae4b1c0
twkwon0417 <kwontony123@konkuk.ac.kr>
2024-08-18 20:08:38 +0900

    feat: jwtController Adive 등록

---
+++ src/main/java/com/kuit/healthmate/global/exception_handler/CustomJwtExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.kuit.healthmate.global.exception_handler;
+
+import com.kuit.healthmate.global.exception.CustomJwtException;
+import com.kuit.healthmate.global.exception.SupplementException;
+import com.kuit.healthmate.global.response.ErrorResponse;
+import com.kuit.healthmate.global.response.ExceptionResponseStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@Order(0)
+@RestControllerAdvice(basePackages = "com/kuit/healthmate")
+public class CustomJwtExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(CustomJwtException.class)
+    public ErrorResponse handle_CustomJwtException(Exception e) {
+        log.error("[handle_CustomJwtException]", e);
+        return new ErrorResponse(ExceptionResponseStatus.JWT_ERROR, e.getMessage()) ;
+    }
+}
