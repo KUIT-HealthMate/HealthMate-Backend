11d6f40bef50fa5ee516b91418db4cac1fa04f69
Park99 <sungung1030@gmail.com>
2024-08-18 20:35:36 +0900

    fix : month-batch restartable

--- src/main/java/com/kuit/healthmate/batch/BatchTestController.java
+++ src/main/java/com/kuit/healthmate/batch/BatchTestController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 public class BatchTestController {
@@ -15,7 +18,7 @@ public class BatchTestController {
     private final JobLauncher jobLauncher;
     private final Job jobMonth;
 
-    public BatchTestController(JobLauncher jobLauncher, @Qualifier("jobMonth") Job jobMonth) {
+    public BatchTestController(JobLauncher jobLauncher, @Qualifier("jobWeek") Job jobMonth) {
         this.jobLauncher = jobLauncher;
         this.jobMonth = jobMonth;
     }
@@ -23,7 +26,10 @@ public class BatchTestController {
     @SneakyThrows
     @GetMapping("/testBatch")
     public String test() {
-        jobLauncher.run(jobMonth, new JobParameters());
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("run.id", System.currentTimeMillis()) // 여기서 JobParameters에 파라미터 추가
+                .toJobParameters();
+        jobLauncher.run(jobMonth, jobParameters);
         return "Job executed successfully";
     }
 }
